de80b8aa80d264425b9f3866f1a4858cd72b118b
Matheus Benevenuto <matthewbene03@gmail.com>
2024-03-07 12:36:53 -0300

    Add Classe Posicao

---
+++ src/boardLayer/Posicao.java
@@ -0,0 +1,32 @@
+package boardLayer;
+
+public class Posicao {
+    private Integer linha;
+    private Integer coluna;
+
+    public Posicao() {
+    }
+
+    public Posicao(Integer linha, Integer coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public Integer getLinha() {
+        return linha;
+    }
+
+    public Integer getColuna() {
+        return coluna;
+    }
+    
+    public void setValor (Integer linha, Integer coluna){
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    @Override
+    public String toString() {
+        return this.linha + ", " + this.coluna;
+    }
+}
