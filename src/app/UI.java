d09a5b13c4e8d251340f959ff1e4fd5ff55c7b44
Matheus Benevenuto <matthewbene03@gmail.com>
2024-03-18 10:59:06 -0300

    Atualizações na classe UI

--- src/app/UI.java
+++ src/app/UI.java
@@ -5,7 +5,7 @@ import Chess.ChessPiece;
 public class UI {
     public static void printBoard (ChessPiece[][] piece){
         for (int i = 0; i < piece.length; i++) {
-            System.out.println((8-i) + " ");
+            System.out.print((8-i) + " ");
             for (int j = 0; j < piece.length; j++) {
                 printPiece(piece[i][j]);
             }
@@ -16,10 +16,10 @@ public class UI {
 
     private static void printPiece(ChessPiece piece){
         if (piece == null) {
-            System.out.println("-");
+            System.out.print("-");
         } else{
-            System.out.println(piece);
+            System.out.print(piece);
         }
-        System.out.println(" ");
+        System.out.print(" ");
     }
 }
